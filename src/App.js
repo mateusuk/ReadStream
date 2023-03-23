d6f21c321a985cb42e0148d895e1f7dd99adb026
Mateus de oliveira <mateus.oliveira.uk@gmail.com>
2023-03-23 14:45:00 +0000

    added search bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import Cards from './components/Cards/Cards'
+import SearchBar from './components/SearchBar/SearchBar'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Cards/>
+      <SearchBar/>
     </div>
   );
 }
---
+++ src/utils/API.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+export const searchBooks = async (searchTerm) => {
+  try {
+    const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&key=AIzaSyBizON8Qx4swlC0a-SRQzgUdwpx7aQzTKo`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
