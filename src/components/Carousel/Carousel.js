4696f7cd69d5720bfcd3ca6f05b79ed419405ff7
Maeve <mariya.kiryakulova@gmail.com>
2023-03-27 14:35:03 +0100

    carousel-import
    Import all required modules/files

---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,14 @@
+import React, { useRef, useState, useEffect } from 'react';
+import { motion } from "framer-motion";
+import './Carousel.css';
+import { Button } from '@mui/material';
+import image1 from "../../assets/img1.jpg";
+import image2 from "../../assets/img2.jpg";
+import image3 from "../../assets/img3.jpg";
+import image4 from "../../assets/img4.jpg";
+import image5 from "../../assets/img5.jpg";
+import image6 from "../../assets/img6.jpg";
+import image7 from "../../assets/img7.jpg";
+import image8 from "../../assets/img8.jpg";
+import image9 from "../../assets/img9.jpg";
+import image10 from "../../assets/img10.jpg";
