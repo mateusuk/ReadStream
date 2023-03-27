fb9cc1de0ad5f38d795ad6f6ba4d89c57c7646e4
Maeve <mariya.kiryakulova@gmail.com>
2023-03-27 14:39:51 +0100

    create-slides-object
    Created object with images and related links

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -12,3 +12,46 @@ import image7 from "../../assets/img7.jpg";
 import image8 from "../../assets/img8.jpg";
 import image9 from "../../assets/img9.jpg";
 import image10 from "../../assets/img10.jpg";
+
+const slides = [
+    {
+        image: image1,
+        article: 'https://www.nytimes.com/2020/12/30/books/great-gatsby-fitzgerald-copyright.html'
+    },
+    {
+        image: image2,
+        article: 'https://www.nytimes.com/2021/10/21/books/james-joyce-ulysses.html?searchResultPosition=6'
+    },
+    {
+        image: image3,
+        article: 'https://www.nytimes.com/2017/05/15/t-magazine/william-friedkin-marcel-proust.html?searchResultPosition=1'
+    },
+    {
+        image: image4,
+        article: 'https://www.nytimes.com/2023/03/11/books/review/hello-beautiful-ann-napolitano.html?searchResultPosition=1'
+    },
+    {
+        image: image5,
+        article: 'https://www.nytimes.com/2016/10/09/books/review/in-exile-with-don-quixote.html?searchResultPosition=5'
+    },
+    {
+        image: image6,
+        article: 'https://www.nytimes.com/2015/06/26/arts/design/looking-at-the-birth-of-lewis-carrolls-alice-150-years-old.html?searchResultPosition=3'
+    },
+    {
+        image: image7,
+        article: 'https://www.nytimes.com/2021/02/24/sports/baseball/thinking-fast-and-slow-book.html?searchResultPosition=42'
+    },
+    {
+        image: image8,
+        article: 'https://www.nytimes.com/2022/07/13/movies/where-the-crawdads-sing-review.html?searchResultPosition=7'
+    },
+    {
+        image: image9,
+        article: 'https://www.nytimes.com/2007/02/11/books/review/Biersdorfer.t.html?searchResultPosition=151'
+    },
+    {
+        image: image10,
+        article: 'https://www.nytimes.com/2020/07/31/books/review-caste-isabel-wilkerson-origins-of-our-discontents.html?searchResultPosition=1'
+    },
+];
