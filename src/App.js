8230e6a1c622dfccccaab320576979fa122a7b25
Maeve <mariya.kiryakulova@gmail.com>
2023-03-27 14:48:25 +0100

    add-carousel-to-app
    Added carousel page to the App and commented

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import SearchBar from './components/SearchBar/SearchBar';
 import Footer from './components/Footer/Footer';
+// import Carousel from './components/Carousel/Carousel';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      {/* <Carousel /> */}
       <SearchBar/>
       <Footer />
     </div>
