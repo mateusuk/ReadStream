19a1689624427ae11f9d1c414cd0a20c961407e1
codri5 <97296166+codri5@users.noreply.github.com>
2023-03-28 15:41:11 +0100

    updated favorites

--- src/components/FavoritePage/FavoritePage.js
+++ src/components/FavoritePage/FavoritePage.js
@@ -1,11 +1,8 @@
-import './FavoritePage.css';
 import { useState, useEffect } from 'react';
 import React from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Cards from '../Cards/Cards';
-
-
 
 function FavoritePage(){
 
@@ -17,26 +14,24 @@ function FavoritePage(){
     }, []);
 
     return(
-        <div className="results-container">
+        <div>
         <Box sx={{ maxWidth: '85%', marginLeft: '12%' }}>
             <Grid
                 container
-                // ref={scrollToRef}
                 spacing={2}
                 direction='row'
                 marginBottom='5rem'
                 marginTop='2.5rem'
                 paddingTop='2rem'
             >
-
                 {favorites.map((item) => (
                     <Grid item xs={3}>
                         <Cards 
                             key={item.id} 
                             title={item.title} 
-                            author={item.authors} 
-                            category={item.categories} 
-                            link={item.previewLink}
+                            author={item.author} 
+                            category={item.category} 
+                            link={item.link}
                             image={item.image}
                         />
                     </Grid>
